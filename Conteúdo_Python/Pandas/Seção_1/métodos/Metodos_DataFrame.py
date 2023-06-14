16a7e60503546907c1e08e73c38246329829c7f7
Rafael_Duque <rafaellduque049@gmail.com>
2023-06-14 15:29:10 -0300

    Modificação de informações

--- Conteúdo_Python/Pandas/Seção_1/métodos/Metodos_DataFrame.py
+++ Conteúdo_Python/Pandas/Seção_1/métodos/Metodos_DataFrame.py
@@ -23,7 +23,17 @@ print('\n', dados[['Nome', 'Idade']]) # Selecionar mais de uma coluna para uso;
 print(dados.get(['Nome', 'Idade', 'Email'])) # Mesmo funcionamento do metodo acima, mas utilizando "get()";
 
 dados.update(('Josias', 21, '122.122.122-12', 'josias.046@gmail.com')) # Adiciona mais dados ao DataFrame;
-print('\n', dados)
+print('\n', dados, '\n')
 
-del dados['CPF'] # Deleta uma tabela;
+new_list = [
+    ('Ana', 21, '122.100.122-12', 'ana.046@gmail.com'),
+    ('Alessando', 21, '122.122.452-12', 'josofws.046@gmail.com'),
+    ('Marquinhos', 21, '122.422.122-12', 'josis.046@gmail.com')
+]
+
+new_list = DataFrame(new_list, columns=['Nome', 'Idade', 'CPF', 'Email'])
+
+dados.update(new_list, overwrite=True)
+
+dados.drop(columns=['CPF'], inplace=True) # Deletar uma coluna e atualizar o DF
 print(dados)
